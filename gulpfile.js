c1dc196c463cb8e7451ce67c9d2cd74a1f2968e8
Marcelo Glacial <glacial@gmail.com>
2017-11-28 11:39:04 -0500

    Add gulp autoreload

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+// Static Server
+gulp.task('serve', function () {
+
+    browserSync.init({
+        server: "./src"
+    });
+
+    gulp.watch("src/*.*").on('change', browserSync.reload);
+});
+gulp.task('default', ['serve']);
