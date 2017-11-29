afd2320ad1283e190e8c161df416ebd63be9ee76
Marcelo Glacial <glacial@gmail.com>
2017-11-29 16:28:38 -0500

    Change structure and add icons

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,5 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-var uglify = require('gulp-uglify');
-var cleanCSS = require('gulp-clean-css');
-var rename = require('gulp-rename');
-var pump = require('pump');
 
 // Static Server
 gulp.task('serve', function () {
@@ -14,24 +10,4 @@ gulp.task('serve', function () {
 
     gulp.watch("src/**/*.*").on('change', browserSync.reload);
 });
-
-// Minify build
-gulp.task('compress', function () {
-    gulp.src('src/assets/js/**/*.js')
-        .pipe(uglify())
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('dist/assets/js'))
-    gulp.src('src/assets/css/**/*.css')
-        .pipe(cleanCSS({
-            compatibility: 'ie8'
-        }))
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('dist/assets/css'));
-})
-
-// Run Default
 gulp.task('default', ['serve']);
