0563deaf26ad524cd68bcb823449e00ea2bdab98
Marcelo Glacial <glacial@gmail.com>
2018-02-15 16:48:56 -0500

    Fix gulp/npm

--- gulpfile.js
+++ gulpfile.js
@@ -3,37 +3,36 @@ var del = require('del');
 var browserSync = require('browser-sync').create();
 var cleanCSS = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
-var minifyHTML = require('gulp-minify-html');
 var pump = require('pump');
 var smushit = require('gulp-smushit');
 var autoprefixer = require('gulp-autoprefixer');
 var gulpSequence = require('gulp-sequence')
 
-// Static Server
+
+
+
+//  Static Server 
 gulp.task('serve', function () {
     browserSync.init({
-        server: "./src"
+        server: {
+            baseDir: "./src/"
+        }
     });
-
-    gulp.watch("src/**/*.*").on('change', browserSync.reload);
 });
 
-// Minimize JS
-gulp.task('build-js', function (cb) {
-    pump([
-            gulp.src('src/assets/js/*'),
-            uglify(),
-            gulp.dest('dist/assets/js/')
-        ],
-        cb
-    );
+// Clean dist and tmp
+gulp.task('build-clean', function () {
+    return del([
+        'dist/**/*'
+    ]);
 });
 
 // Copy files to dist
-gulp.task('build-copy', function () {
+gulp.task('build-dist', function () {
     return gulp.src('src/**/*')
         .pipe(gulp.dest('dist/'));
 });
+
 
 // Minify CSS and ADD vendor prefix
 gulp.task('build-css', function () {
@@ -51,6 +50,18 @@ gulp.task('build-css', function () {
         .pipe(gulp.dest('dist/assets/css'));
 });
 
+// Minimize JS
+gulp.task('build-js', function (cb) {
+    pump([
+        gulp.src('src/assets/js/*.js'),
+        uglify(),
+        gulp.dest('dist/assets/js')
+    ],
+        cb
+    );
+});
+
+
 // Optimize images
 gulp.task('build-img', function () {
     return gulp.src('src/assets/img/*')
@@ -58,43 +69,12 @@ gulp.task('build-img', function () {
         .pipe(gulp.dest('dist/assets/img'))
 });
 
-// Serve dist files
-gulp.task('build-serve', function () {
-    browserSync.init({
-        server: "./dist"
-    })
-});
-
-// Clean dist and tmp
-gulp.task('build-clean', function () {
-    return del([
-        'dist/**/*',
-        'tmp/**/*'
-    ]);
-});
-
-// Minify XHTML
-gulp.task('build-html', function () {
-    var opts = {
-        comments: false,
-        spare: true
-    };
-
-    gulp.src('./src/**/*.html')
-        .pipe(minifyHTML(opts))
-        .pipe(gulp.dest('./dist/'))
-});
 
 // Build
 gulp.task('build', gulpSequence(
     'build-clean',
-    'build-copy',
+    'build-dist',
     'build-css',
     'build-js',
-    'build-html',
-    'build-img',
-    'build-serve'
+    'build-img'
 ));
-
-// Run Default
-gulp.task('default', ['serve']);
