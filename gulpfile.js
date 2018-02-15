f93c73e505221ef9ac7e9bac9c026c42f6ced270
Marcelo Glacial <glacial@gmail.com>
2018-02-15 16:48:56 -0500

    Revert "Fix gulp/npm"
    This reverts commit 0563deaf26ad524cd68bcb823449e00ea2bdab98.

--- gulpfile.js
+++ gulpfile.js
@@ -3,36 +3,37 @@ var del = require('del');
 var browserSync = require('browser-sync').create();
 var cleanCSS = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
+var minifyHTML = require('gulp-minify-html');
 var pump = require('pump');
 var smushit = require('gulp-smushit');
 var autoprefixer = require('gulp-autoprefixer');
 var gulpSequence = require('gulp-sequence')
 
-
-
-
-//  Static Server 
+// Static Server
 gulp.task('serve', function () {
     browserSync.init({
-        server: {
-            baseDir: "./src/"
-        }
+        server: "./src"
     });
+
+    gulp.watch("src/**/*.*").on('change', browserSync.reload);
 });
 
-// Clean dist and tmp
-gulp.task('build-clean', function () {
-    return del([
-        'dist/**/*'
-    ]);
+// Minimize JS
+gulp.task('build-js', function (cb) {
+    pump([
+            gulp.src('src/assets/js/*'),
+            uglify(),
+            gulp.dest('dist/assets/js/')
+        ],
+        cb
+    );
 });
 
 // Copy files to dist
-gulp.task('build-dist', function () {
+gulp.task('build-copy', function () {
     return gulp.src('src/**/*')
         .pipe(gulp.dest('dist/'));
 });
-
 
 // Minify CSS and ADD vendor prefix
 gulp.task('build-css', function () {
@@ -50,18 +51,6 @@ gulp.task('build-css', function () {
         .pipe(gulp.dest('dist/assets/css'));
 });
 
-// Minimize JS
-gulp.task('build-js', function (cb) {
-    pump([
-        gulp.src('src/assets/js/*.js'),
-        uglify(),
-        gulp.dest('dist/assets/js')
-    ],
-        cb
-    );
-});
-
-
 // Optimize images
 gulp.task('build-img', function () {
     return gulp.src('src/assets/img/*')
@@ -69,12 +58,43 @@ gulp.task('build-img', function () {
         .pipe(gulp.dest('dist/assets/img'))
 });
 
+// Serve dist files
+gulp.task('build-serve', function () {
+    browserSync.init({
+        server: "./dist"
+    })
+});
+
+// Clean dist and tmp
+gulp.task('build-clean', function () {
+    return del([
+        'dist/**/*',
+        'tmp/**/*'
+    ]);
+});
+
+// Minify XHTML
+gulp.task('build-html', function () {
+    var opts = {
+        comments: false,
+        spare: true
+    };
+
+    gulp.src('./src/**/*.html')
+        .pipe(minifyHTML(opts))
+        .pipe(gulp.dest('./dist/'))
+});
 
 // Build
 gulp.task('build', gulpSequence(
     'build-clean',
-    'build-dist',
+    'build-copy',
     'build-css',
     'build-js',
-    'build-img'
+    'build-html',
+    'build-img',
+    'build-serve'
 ));
+
+// Run Default
+gulp.task('default', ['serve']);
